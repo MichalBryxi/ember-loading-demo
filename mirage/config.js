9c39e038a5fa906a370ecad0c001e2daa1668896
Michal Bryxí <michal.bryxi@gmail.com>
2016-05-25 11:27:25 +0100

    Some loading magic

--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,4 @@
 export default function() {
   this.namespace = '/api';    // make this `api`, for example, if your API is namespaced
-  this.get('/slows');
+  this.get('/slows', { timing: 3000 });
 }
--- mirage/factories/slow.js
+++ mirage/factories/slow.js
@@ -1,7 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  name() {
+  name(i) {
     return faker.name.firstName();
   }
 });
